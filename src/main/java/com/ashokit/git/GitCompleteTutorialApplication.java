69d1f47791c35202b5861325011a36e9c732e8ad
Sagar <sagarkmohapatra2012@gmail.com>
2021-07-20 12:54:45 +0530

    first commit

---
+++ src/main/java/com/ashokit/git/GitCompleteTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.ashokit.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCompleteTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitCompleteTutorialApplication.class, args);
+		System.out.println("Running");
+	}
+
+}
